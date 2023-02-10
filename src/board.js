724607b8e55baaf1bff8e806f2878d2a3b6e5935
Flyspeck101 <lawrence.chen8@education.nsw.gov.au>
2023-02-10 18:19:19 +1100

    Update board.js

--- src/board.js
+++ src/board.js
@@ -1,8 +1,11 @@
-import * as Mino from "./mino.js";
+import * from "./mino.js";
 
 function Board() {
   let board = [];
-  for (let i = 0; i < 600; i++) board.push(Mino.Mino.NONE);
+  for (let i = 0; i < 600; i++) board.push(Mino.NONE);
+  board.onLeftWall = tile => tile % 10 == 0;
+  board.onRightWall = tile => tile % 10 == 9;
+  board.onBottomWall = tile => tile < 10;
   return board;
 }
 
