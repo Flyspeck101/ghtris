b00a85d02b0916db9dda353d9edf94e2e3986c22
Flyspeck101 <yeetersdeleters12345@gmail.com>
2023-02-11 16:08:16 +1100

    Update pieces.js

--- src/pieces.js
+++ src/pieces.js
@@ -1,2 +1,22 @@
-let Piece = {
-  "Z": 
+import ( Mino } from "mino.js";
+let Piece = {}
+
+Mino.keys.forEach(element => {
+  if (Mino[element] == 0 || Mino[element] == 8) return;
+  Piece[element] = {
+    "mino": Mino[element]
+    create() {
+      return {
+        "rotations": {},
+      "rotation": 0
+      
+      rotate(rotationAmount) {
+        // TODO: Rotation code 
+        this["rotation"] = (this["rotation"] + rotationAmount) % 4;
+      }
+   }
+});
+
+export { Piece };
+      
+    
